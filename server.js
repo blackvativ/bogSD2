aa2560f61e239d08bfcf2e642c6716d19e6def66
Glitch (bog-test) <none>
2025-06-26 21:03:39 +0000

    💷🍏 Checkpoint ./server.js:80793987/15318

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fetch = require =("node-fetch");
+const fetch = require("node-fetch"); // Corrected: removed extra "="
 const cors = require("cors");
 
 const app = express();
